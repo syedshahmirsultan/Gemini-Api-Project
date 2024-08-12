1ad6d217195ca6f3430b81bb7f31f13faf8c9073
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2024-08-13 02:29:57 +0500

    Update NavBar.tsx

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <div className='p-6 md:top-0 md:sticky z-20'>
+        <div className='p-6 z-20'>
             {/* Logo*/}
         <h1 className='text-2xl cursor-pointer md:text-3xl font-semibold tracking-wide text-red-500'>Medi<span className='text-green-500'>tail</span></h1>
         </div>
