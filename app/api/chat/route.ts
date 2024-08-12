68f984ffd1d2c5bdd6569da4da55385182cbf187
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-08-13 02:06:55 +0500

    change model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const geminiStream = await genAI
-    .getGenerativeModel({ model: 'gemini-1.5-flash-latest' })
+    .getGenerativeModel({ model: 'models/gemini-1.5-pro-latest' })
     .generateContentStream(buildGoogleGenAIPrompt(messages));
 
   // Convert the response into a friendly text-stream
