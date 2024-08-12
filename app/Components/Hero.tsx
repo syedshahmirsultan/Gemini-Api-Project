fbe9d1f59eeec9ecf8524cfac87f5ed1fad7acd0
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-08-13 01:30:45 +0500

    resolve error

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
     <div className='mt-8 md:mt-16 p-5 md:p-0 w-full md:max-w-5xl mx-auto'>
             <h1 className='text-xl md:text-3xl text-white font-semibold'><span className='tracking-wide text-red-500'>Medi</span><span className=' tracking-wide text-green-500'>tail </span>
-            is a pharmacistical assistant helps determine if your medication is suitable for your health and suggests whether it's the best option for your specific condition or not.</h1>
+            is a pharmacistical assistant helps determine if your medication is suitable for your health and suggests whether its the best option for your specific condition or not.</h1>
         </div>
     );
 }
